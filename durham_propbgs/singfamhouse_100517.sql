771ed7e4525d6e8803ae12eae09b28b3559dee4c
Jim McManus <jmpmcmanus@flatapps.net>
2018-02-20 11:33:19 -0500

    Updated propsalescompass_100517.sql singfamhouse_100517.sql

--- durham_propbgs/singfamhouse_100517.sql
+++ durham_propbgs/singfamhouse_100517.sql
@@ -1,2 +1,2 @@
-\copy datahub_be_app_singfamhouse_bg FROM '/home/jmcmanus/code_for_durham/affordable_housing/data/propmap/singfamhouse_bgs_100517.csv' DELIMITER ',' CSV;
-\copy datahub_be_app_singfamhouse_hd FROM '/home/jmcmanus/code_for_durham/affordable_housing/data/propmap/singfamhouse_hds_100517.csv' DELIMITER ',' CSV;
+\copy datahub_be_app_singfamhouse_bg FROM '/home/jmcmanus/code_for_durham/affordable_housing/virtualenv/development/datahub-dw/durham_propbgs/singfamhouse_bgs_100517.csv' DELIMITER ',' CSV;
+\copy datahub_be_app_singfamhouse_hd FROM '/home/jmcmanus/code_for_durham/affordable_housing/virtualenv/development/datahub-dw/durham_propbgs/singfamhouse_hds_100517.csv' DELIMITER ',' CSV;
