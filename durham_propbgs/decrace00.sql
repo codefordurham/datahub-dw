6889f8be41eaa9605688b705e464bb0106dd7e94
Jim McManus <jmpmcmanus@flatapps.net>
2018-03-30 15:26:34 -0400

    Made corrections

--- durham_propbgs/decrace00.sql
+++ durham_propbgs/decrace00.sql
@@ -1,3 +1,3 @@
-\copy decrace_bgs_2000(id,pop_00,ptwhnl_00,ptblknl_00,ptnanl_00,ptasnl_00,ptpanl_00,ptothnl_00,pt2mnl_00,ptlat_00) FROM '/home/jmcmanus/code_for_durham/affordable_housing/virtual/development/datahub-dw/durham_propbgs/dec_race_2000.csv' WITH DELIMITER ',' CSV HEADER;
+\copy datahub_be_app_decrace_bgs_00(id,pop_00,ptwhnl_00,ptblknl_00,ptnanl_00,ptasnl_00,ptpanl_00,ptothnl_00,pt2mnl_00,ptlat_00) FROM '/home/jmcmanus/code_for_durham/affordable_housing/virtualenv/development/datahub-dw/durham_propbgs/dec_race_2000.csv' WITH DELIMITER ',' CSV HEADER;
 
 COMMIT;
