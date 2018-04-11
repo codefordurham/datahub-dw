ed746491a32cb2dd83ca2fb93950d789071a7166
Jim McManus <jmpmcmanus@flatapps.net>
2018-04-10 21:49:32 -0400

    Updated sql and csv files

--- durham_propbgs/acsincome16.sql
+++
@@ -1,3 +0,0 @@
-\copy acsincome_bgs_2016(geoid10,mhi,mhi_ah,mnhi,mgr_phi,mmoc_phi) FROM '/home/jmcmanus/code_for_durham/affordable_housing/virtual/development/datahub-dw/durham_propbgs/acs_income_2016.csv' WITH DELIMITER ',' CSV HEADER;
-
-COMMIT;
--- durham_propbgs/propsalesacs_2017.sql
+++ durham_propbgs/propsalesacs_2017.sql
@@ -1,1 +1,1 @@
-\copy datahub_be_app_propsales17(id,meansp,minsp,maxsp,mediansp,totsp,nums,mhi,pir,mgr_phi,mmoc_phi) FROM '/home/jmcmanus/code_for_durham/affordable_housing/data/propmap/propsalesacs_2017_bgs.csv' WITH DELIMITER ',' CSV HEADER;
+\copy datahub_be_app_propsales17(id,meansp,minsp,maxsp,mediansp,totsp,nums,mhi,pir,mgr_phi,mmoc_phi) FROM '/home/jmcmanus/code_for_durham/affordable_housing/virtualenv/development/datahub-dw/durham_propbgs/propsalesacs_2017_bgs.csv' WITH DELIMITER ',' CSV HEADER;
